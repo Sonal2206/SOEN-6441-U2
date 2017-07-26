ce57ff4d514107cfb8f7da8bedce6e0ec9cfda62
sonal <sonalsharma9222@gmail.com>
2017-07-26 00:48:54 -0400

    Created Controller Class New DTD file generated Handled Exceptions Unit Testing

--- Incarnation2.py
+++ Incarnation2.py
@@ -1,17 +1,17 @@
+
+from XmlGenerator import XmlGenerator
 import math
-# This function gives the length of the segment X1X2 as described in the specification of Cheers!
+
+
 # Formula for calculating length of the segment is l = 2R(1 – cos(α/2)),
 from CalculateAlpha import CalculateAlpha
 
+radius = float(input("Enter the radius of the circle :"))
+# creating instance of class CalculateAlpha
+alpha_calculator = CalculateAlpha()
+distance = 2 * radius * (1 - math.cos(alpha_calculator.get_alpha() / 2.0))
+print("The length of the segment X1-X2 is", distance, "units")
 
-def get_length():
-    radius = float(input("Enter the radius of the circle :"))
-    alpha_calculator = CalculateAlpha();
-    distance = 2 * radius * (1 - math.cos(alpha_calculator.get_alpha() / 2.0))
-    print("The length of the segment X1-X2 is", distance, "units")
-
-
-get_length()
 
 
 
